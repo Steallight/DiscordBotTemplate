a132f3f53dd885a1a7176f8f9acf5b8ca4ceb2d7
Steallight <69077005+Steallight@users.noreply.github.com>
2026-01-21 15:37:11 +0100

    updated everything to newest JDA and removed old Engine

--- src/main/java/de/steallight/testbot/commands/socials.java
+++ src/main/java/de/steallight/testbot/commands/socials.java
@@ -1,52 +1,38 @@
 package de.steallight.testbot.commands;
 
-import de.azraanimating.maddoxengine.handling.command.Command;
-import de.azraanimating.maddoxengine.handling.command.CommandEvent;
-import de.azraanimating.maddoxengine.handling.objects.MaddoxGuild;
-import de.azraanimating.maddoxengine.handling.objects.MaddoxMember;
+import de.steallight.testbot.main.Bot;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.awt.*;
-import java.util.concurrent.TimeUnit;
 
-public class socials extends Command {
-
-    public socials(){
-        this.setName("socials");
-    }
+public class socials extends ListenerAdapter {
 
     String instagram = "https://instagram.com/";
-    String Twitch = "https://twitch.tv/";
+    String twitchURL = "https://twitch.tv/";
 
     @Override
-    protected void execute(CommandEvent event, MaddoxMember sender, MaddoxGuild server) {
-        EmbedBuilder info = new EmbedBuilder();
-        EmbedBuilder insta = new EmbedBuilder();
-EmbedBuilder twitch = new EmbedBuilder();
+    public void onMessageReceived(MessageReceivedEvent e) {
+        if (e.getMessage().getContentRaw().equals(Bot.PREFIX+"socials")){
+            EmbedBuilder info = new EmbedBuilder();
+            EmbedBuilder insta = new EmbedBuilder();
+            EmbedBuilder twitch = new EmbedBuilder();
 
-        info.setTitle("Meine Socials");
-        info.setColor(Color.WHITE);
+            info
+                    .setTitle("Meine Socials")
+                    .setColor(Color.WHITE);
+            insta
+                    .setTitle("Instagram", instagram+"steallight")
+                    .setColor(Color.decode("#E1306C"))
+                    .setThumbnail("https://cdn.icon-icons.com/icons2/1826/PNG/512/4202090instagramlogosocialsocialmedia-115598_115703.png");
 
+            twitch
+                    .setTitle("Twitch", twitchURL+"yugusaa")
+                    .setColor(Color.decode("#9933ff"))
+                    .setThumbnail("https://media-exp1.licdn.com/dms/image/C560BAQHm82ECP8zsGw/company-logo_200_200/0?e=2159024400&v=beta&t=r94Gy3UzJ4RMyHVIeECB9Q67gfAFC_FZVq9uCenVHXs");
 
-        //Instagram
-        insta.setTitle("Instagram", instagram+ "steallightmusic");
-
-        insta.setThumbnail("https://cdn.icon-icons.com/icons2/1826/PNG/512/4202090instagramlogosocialsocialmedia-115598_115703.png");
-        insta.setColor(Color.decode("#E1306C"));
-
-
-
-
-        //Twitch
-
-
-        twitch.setTitle("Twitch", Twitch + "steallight");
-        twitch.setThumbnail("https://media-exp1.licdn.com/dms/image/C560BAQHm82ECP8zsGw/company-logo_200_200/0?e=2159024400&v=beta&t=r94Gy3UzJ4RMyHVIeECB9Q67gfAFC_FZVq9uCenVHXs");
-        twitch.setColor(Color.decode("#9933ff"));
-
-        event.reply(info.build());
-        event.reply(insta.build());
-        event.reply(twitch.build());
-        event.deleteEventMessage();
+            e.getChannel().asTextChannel().sendMessageEmbeds(info.build(),insta.build(),twitch.build()).queue();
+        }
     }
 }
--- src/main/java/de/steallight/testbot/listener/SupportListener.java
+++ src/main/java/de/steallight/testbot/listener/SupportListener.java
@@ -1,83 +1,70 @@
 package de.steallight.testbot.listener;
 
-import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.components.actionrow.ActionRow;
+import net.dv8tion.jda.api.components.buttons.Button;
 import net.dv8tion.jda.api.entities.*;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceMoveEvent;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
-import net.dv8tion.jda.api.exceptions.RateLimitedException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.requests.RestAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
+import java.awt.*;
 
 public class SupportListener extends ListenerAdapter {
 
     String channelid = "655373012131905550";
     String textid = "773841319805452318";
 
+    private static final Logger logger = LoggerFactory.getLogger(SupportListener.class);
 
-    @Override
-    public void onGuildVoiceJoin(GuildVoiceJoinEvent e) {
-        String user = e.getMember().getUser().getName();
+    public void onGuildVoiceUpdate(GuildVoiceUpdateEvent e) {
+        try {
+            String supportWarteraumID = channelid;
+            System.out.println("Support Warteraum ID: " + supportWarteraumID);
+
+            // Null-Check: Wurde ein Channel betreten?
+            AudioChannelUnion channelJoined = e.getChannelJoined();
+            if (channelJoined == null) {
+                // User hat Channel verlassen, nicht betreten
+                return;
+            }
+
+            // Prüfe ob der richtige Channel betreten wurde
+            if (channelJoined.getId().equals(supportWarteraumID)) {
+                TextChannel notifyChannel = e.getGuild().getTextChannelById("1279592048751804436");
+
+                if (notifyChannel == null) {
+                    System.err.println("Notify Channel nicht gefunden!");
+                    return;
+                }
+
+                String userID = e.getMember().getId();
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setTitle("Jemand wartet im Support-Warteraum")
+                        .setDescription(e.getMember().getAsMention())
+                        .setColor(Color.ORANGE);
+
+                net.dv8tion.jda.api.components.buttons.Button acceptBtn = Button.success("accept:supportWarteraum_" + userID, "Annehmen");
+
+                Role supRole = e.getGuild().getRoleById("729280923207729154");
+                if (supRole != null){
+                    notifyChannel.sendMessage(supRole.getAsMention())
+                            .addComponents(ActionRow.of(acceptBtn))
+                            .addEmbeds(eb.build())
+                            .queue();
+                }
 
 
-        if (e.getChannelJoined().getId().equalsIgnoreCase(channelid)) {
-
-
-            String name = e.getMember().getUser().getName();
-            Guild guild = e.getGuild();
-
-            Role admin = e.getGuild().getRoleById("655363884978405376");
-            Role mods = e.getGuild().getRoleById("662338553895845918");
-List usejoined = e.getMember().getRoles();
-            TextChannel tc = e.getGuild().getTextChannelById(textid);
-
-
-            EmbedBuilder eb = new EmbedBuilder();
-            eb.setTitle(e.getMember().getUser().getName() + " befindet sich im Warteraum");
-            eb.addField("User", e.getMember().getUser().getAsMention(), true);
-
-
-
-
-            tc.sendMessage(eb.build()).complete().delete().queueAfter(10, TimeUnit.MINUTES);
-            tc.sendMessage(admin.getAsMention()+" "+ mods.getAsMention()).complete().delete().queueAfter(10, TimeUnit.MINUTES);
-
-
+            }
+        } catch (Exception ex) {
+            logger.error("Fehler beim Laden der Config");
         }
     }
 
-    @Override
-    public void onGuildVoiceMove(GuildVoiceMoveEvent e) {
 
-        if (e.getChannelJoined().getId().equalsIgnoreCase(channelid)) {
-
-
-            String name = e.getMember().getUser().getName();
-            Guild guild = e.getGuild();
-
-            Role supporter = e.getGuild().getRoleById("655363884978405376");
-
-            TextChannel tc = e.getGuild().getTextChannelById(textid);
-
-
-            EmbedBuilder eb = new EmbedBuilder();
-            eb.setTitle(e.getMember().getUser().getName() + " befindet sich im Warteraum");
-            eb.addField("User", e.getMember().getUser().getAsMention(), true);
-            eb.addField("Rolle", supporter.getAsMention(), false);
-
-
-            tc.sendMessage(eb.build()).queue();
-
-
-        }
-    }
 }
 
 
