b8cffbbbc571cd50745e24670349b4df3b710dc1
Luca <steallight.lp@gmail.com>
2026-01-31 13:36:23 +0100

    Add dotenv support for environment variable management; update Bot class to use token from .env file

--- src/main/java/de/steallight/testbot/main/Bot.java
+++ src/main/java/de/steallight/testbot/main/Bot.java
@@ -2,18 +2,24 @@ package de.steallight.testbot.main;
 
 
 import de.steallight.testbot.commands.*;
+import de.steallight.testbot.listener.AvatarEmbedder;
 import de.steallight.testbot.listener.SupportListener;
 import de.steallight.testbot.listener.VoiceListener;
+import de.steallight.testbot.slash.AvatarSlashCMD;
+import de.steallight.testbot.slash.SocialsSlashCMD;
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import javax.security.auth.login.LoginException;
 
 public class Bot {
-
+    private final Dotenv dotenv;
 
     public static String PREFIX = "!";
     public static JDA jda;
@@ -21,16 +27,15 @@ public class Bot {
 
 
     public Bot() throws LoginException, InterruptedException {
+        this.dotenv = Dotenv.configure()
+                .directory("./")
+                .load();
         INSTANCE = this;
 
 
-        // Token aus Umgebungsvariable lesen (sicherer als hardcodiert)
-        String token = System.getenv("BOT_TOKEN");
-        if (token == null || token.isBlank()) {
-            throw new IllegalStateException("Discord token is not set. Please provide BOT_TOKEN environment variable.");
-        }
 
-        jda = JDABuilder.create(token,
+
+        jda = JDABuilder.create(dotenv.get("TOKEN"),
                 GatewayIntent.GUILD_MESSAGES,
                 GatewayIntent.DIRECT_MESSAGES,
                 GatewayIntent.GUILD_MODERATION,
@@ -40,7 +45,8 @@ public class Bot {
                 GatewayIntent.DIRECT_MESSAGE_REACTIONS,
                 GatewayIntent.DIRECT_MESSAGE_TYPING,
                 GatewayIntent.GUILD_EXPRESSIONS,
-                GatewayIntent.GUILD_MESSAGE_REACTIONS
+                GatewayIntent.GUILD_MESSAGE_REACTIONS,
+                GatewayIntent.MESSAGE_CONTENT
         ).enableCache(
                 CacheFlag.VOICE_STATE,
                 CacheFlag.MEMBER_OVERRIDES,
@@ -49,7 +55,7 @@ public class Bot {
                 CacheFlag.ACTIVITY
         ).build();
 
-        Guild server = jda.awaitReady().getGuildById("");
+        Guild server = jda.awaitReady().getGuildById("645388418062221312");
         assert server != null;
 
 
@@ -77,6 +83,7 @@ public class Bot {
      * wir "this-escape"-Warnungen beim Aufruf aus dem Konstruktor.
      */
     private void addEvents() {
+        //Prefix Commands
         jda.addEventListener(new announceCMD());
         jda.addEventListener(new Avatar());
         jda.addEventListener(new ClearCommand());
@@ -88,12 +95,22 @@ public class Bot {
         jda.addEventListener(new socials());
         jda.addEventListener(new SupportListener());
         jda.addEventListener(new VoiceListener());
+        //Slash Commands
+        jda.addEventListener(new AvatarSlashCMD());
+        jda.addEventListener(new SocialsSlashCMD());
+        jda.addEventListener(new AvatarEmbedder());
     }
 
     //Für Slash Commands
     private void updateCommands(Guild server) {
         server.updateCommands()
                 .addCommands(
+                        Commands.slash("avatar", "Gibt die Avatar-URL des erwähnten Members zurück")
+                                .addOption(OptionType.USER, "user", "Der Benutzer, dessen Avatar du sehen möchtest", true),
+
+                        Commands.slash("socials", "Gibt die Social-Media-Kanäle des Entwicklers aus"),
+                        Commands.slash("avatarembed", "Erstellt ein Bild mit dem Avatar des angegebenen Benutzers eingebettet")
+                                .addOption(OptionType.USER, "user", "Der Benutzer, dessen Avatar eingebettet werden soll", true)
                         /*Commands.slash("set-ticket", "Setzte den TicketChannel")
                                 .setDefaultPermissions(DefaultMemberPermissions.DISABLED) <------- Entscheidet ob nur Admins oder jeder den Befehl sehen kann
                                 .addOption(OptionType.CHANNEL, "ticketchannel", "Bitte wähle den Channel der als Ticket Channel gesetzt werden soll", true),
